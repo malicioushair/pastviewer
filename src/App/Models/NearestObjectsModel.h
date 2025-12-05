3e7545e4be05dadf62368e520627a059aa7731bf
Dmitry Vorobyov <d.vorobyov@movavi.om>
2025-12-06 00:45:00 +0100

    Introduce a filtering model

--- src/App/Models/NearestObjectsModel.h
+++ src/App/Models/NearestObjectsModel.h
@@ -1,35 +1,22 @@
 #pragma once
 
-#include <QAbstractListModel>
 #include <QGeoCoordinate>
 #include <QGeoPositionInfoSource>
+#include <QSortFilterProxyModel>
 #include <QVariant>
+#include <QtCore/qabstractitemmodel.h>
 
 #include "App/Utils/NonCopyMovable.h"
 
 class ScreenObjectsModel;
 
 class NearestObjectsModel
-	: public QAbstractListModel
+	: public QSortFilterProxyModel
 {
 	Q_OBJECT
 
 public:
-	enum Roles
-	{
-		// Getters
-		Coordinate = Qt::UserRole + 1,
-		Title,
-		Photo,
-		Thumbnail,
-		Bearing,
-		Year,
-
-		// Setters
-		Selected,
-	};
-
-	explicit NearestObjectsModel(ScreenObjectsModel * sourceModel, QGeoPositionInfoSource * positionSource, QObject * parent = nullptr);
+	explicit NearestObjectsModel(QAbstractListModel * sourceModel, QGeoPositionInfoSource * positionSource, QObject * parent = nullptr);
 	NON_COPY_MOVABLE(NearestObjectsModel);
 
 	~NearestObjectsModel();
@@ -39,18 +26,17 @@ public:
 signals:
 	void CountChanged();
 
-public:
-	int rowCount(const QModelIndex & parent = QModelIndex()) const override;
-	QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const override;
-	bool setData(const QModelIndex & index, const QVariant & value, int role) override;
-	QHash<int, QByteArray> roleNames() const override;
+protected:
+	bool filterAcceptsRow(int source_row, const QModelIndex & source_parent) const override;
+	bool lessThan(const QModelIndex & left, const QModelIndex & right) const override;
 
 private slots:
 	void OnPositionUpdated(const QGeoPositionInfo & info);
 	void OnSourceModelChanged();
 
 private:
-	void UpdateFilteredItems();
+	void UpdateAcceptedRows();
+	double GetDistance(int sourceRow) const;
 
 	struct Impl;
 	std::unique_ptr<Impl> m_impl;
